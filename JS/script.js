044d3a37a7b82751da0317c4b7df5d39780cf0b0
Matias Abut <mat.abut@hotmail.com>
2022-07-20 14:47:50 -0300

    Agregando Arrays

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,35 @@
-let nombreProductoA = "Caramelos";
-let precioProductoA = 3;
-let stockProductoA = 1000;
+function Producto (nombre, precio, stock){
+    this.nombre = nombre
+    this.precio = precio
+    this.stock = stock
+    this.sumarIva = function (precio){
+        return precio * 0.21
+    }
+    this.sumarStock = function (cantidad) {this.stock += cantidad}
+}
 
-let nombreProductoB = "Chupetines";
-let precioProductoB = 15;
-let stockProductoB = 200;
+const productoA = new Producto("Caramelos", 3, 1000)
+const productoB = new Producto("Chupetines", 15, 200)
+const productoC = new Producto("Galletitas", 70, 30)
+const productoD = new Producto("Chocolates", 100, 50)
+const productoE = new Producto("Gomitas", 2, 2000)
+const productoF = new Producto("Bombones", 25, 150)
+const productoG = new Producto("Snacks", 120, 50)
+const productoH = new Producto("Alfajores", 80, 100)
+const productoI = new Producto("Barritas", 150, 30)
+const productoJ = new Producto("Chicles", 10, 100)
 
-let nombreProductoC= "Galletitas";
-let precioProductoC = 70;
-let stockProductoC = 30;
+let listaProductos = [productoA, productoB, productoC, productoD, productoE, productoF, productoG, productoH, productoI, productoJ]
 
-let nombreProductoD = "Chocolates";
-let precioProductoD = 100;
-let stockProductoD = 50;
+let nombresProductos = []
 
-let nombreProductoE = "Gomitas";
-let precioProductoE = 2;
-let stockProductoE = 2000;
+function listaProductosS(){
+    for(const producto of listaProductos){
+        nombresProductos.push(producto.nombre)
+    }
+}
 
-let nombreProductoF = "Bombones";
-let precioProductoF = 25;
-let stockProductoF = 150;
-
-let nombreProductoG = "Snacks";
-let precioProductoG = 120;
-let stockProductoG = 50;
-
-let nombreProductoH = "Alfajores";
-let precioProductoH = 80;
-let stockProductoH = 100;
-
-let nombreProductoI = "Barritas";
-let precioProductoI = 150;
-let stockProductoI = 30;
-
-let nombreProductoJ = "Chicles";
-let precioProductoJ = 10;
-let stockProductoJ = 100;
+listaProductosS()
 
 alert("Bienvenido a La Carameleria! 😁")
 
@@ -54,65 +47,63 @@ function calculoPrecio(cantidad, precio) {
     precioTotal += cantidad * precio;
 }
 
-function calculoStock(cantidad, stock, precio){
-        if(stock >= cantidad){
-            calculoPrecio(cantidad, precio)
-            alert("El producto total es de: $" + (cantidad * precio))
+function calculoStock(cantidad, producto){
+        if(producto.stock >= cantidad){
+            calculoPrecio(cantidad, producto.precio)
+            alert("El producto total es de: $" + (cantidad * producto.precio))
         }
         else{
-            alert("No disponemos de esa cantidad en stock. Nuestro stock actual es de: " + stock + " unidades")
+            alert("No disponemos de esa cantidad en stock. Nuestro stock actual es de: " + producto.stock + " unidades")
         }
 }
 
-function sumaIva (precio){
-    return precio * 0.21
-}
+
 
 for (let i = 0; i < cantidad; i++) {
 
-let compra = prompt("Ingrese el nombre del producto que quiere comprar:  \n- Caramelos 🍬 \n- Chupetines 🍭 \n -Galletitas 🍪 \n -Chocolates 🍫 \n -Gomitas 🍡 \n -Bombones 🌰 \n -Snacks 🥜 \n -Alfajores 🍩 \n -Barritas 🥖 \n -Chicles 🍬").toLowerCase()
+let compra = prompt("Ingrese el nombre del producto que quiere comprar: \n" + nombresProductos.join("\n")).toLowerCase()
 
 let cantidad1 = prompt("Ingrese la cantidad del producto que quiere comprar:")
 
 switch (compra) {
     case "Caramelos".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoA, precioProductoA)
+    calculoStock(cantidad1, productoA)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     case "Chupetines".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoB, precioProductoB)
+    calculoStock(cantidad1, productoB)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     case "Galletitas".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoB, precioProductoC)
+    calculoStock(cantidad1, productoC)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     case "Chocolates".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoC, precioProductoD)
+    calculoStock(cantidad1, productoD)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     case "Gomitas".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoE, precioProductoE)
+    calculoStock(cantidad1, productoE)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     case "Bombones".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoF, precioProductoF)
+    calculoStock(cantidad1, productoF)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     case "Snacks".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoG, precioProductoG)
+    calculoStock(cantidad1, productoG)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     case "Alfajores".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoH, precioProductoH)
+    calculoStock(cantidad1, productoH)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     case "Barritas".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoI, precioProductoI)
+    calculoStock(cantidad1, productoI)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     case "Chicles".toLowerCase(): 
-    calculoStock(cantidad1, stockProductoJ, precioProductoJ)
+    calculoStock(cantidad1, productoJ)
     alert ("Selecciono " + cantidad1 + " " + compra)
     break;
     default:
@@ -125,29 +116,33 @@ switch (true){
     case precioTotal <= 200 && precioTotal >= 100:
         precioTotal = precioTotal * 0.95
         alert("Recibiste un descuento del 5% por tu compra")
+        alert("Gracias por su compra! Vuelva pronto!")
         break;
     case precioTotal <= 500 && precioTotal > 200:
         precioTotal = precioTotal * 0.85
         alert("Recibiste un descuento del 15% por tu compra")
+        alert("Gracias por su compra! Vuelva pronto!")
         break;
     case precioTotal <= 700 && precioTotal > 500:
         precioTotal = precioTotal * 0.75
         alert("Recibiste un descuento del 25% por tu compra")
+        alert("Gracias por su compra! Vuelva pronto!")
         break;
     case precioTotal > 700:
         precioTotal = precioTotal * 0.50
         alert("Recibiste un descuento del 50% por tu compra")
+        alert("Gracias por su compra! Vuelva pronto!")
         break;
     default:
         console.log(precioTotal)
-        alert("No hay descuentos porque su compra es menor de $100");
+        alert("No hay descuentos porque su compra es menor de $100")
+        alert("Gracias por su compra! Vuelva pronto!")
         break;
 }
 
-let precioTotalConImpuestos = sumaIva(precioTotal)
+// let precioTotalConImpuestos = sumaIva(precioTotal)
 
 alert("Precio total con impuestos es: " + precioTotalConImpuestos)
 
-alert("Gracias por su compra! Vuelva pronto!")
 
 
