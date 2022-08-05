a481c66829f69d17acbc0745c819894039525866
ignacioborraz <ignacioborraz@hotmail.com>
2022-08-04 22:41:59 -0300

    carrito

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,56 +1,39 @@
-let carrito = []
-
-function addToCartClicked() {
+function showProducts() {
+    let ids = []
     if (localStorage.getItem('carrito')) { //si existe el carrito en el almacenamiento
-        carrito = JSON.parse(localStorage.getItem('carrito')) //si existe lo defino
-
+        ids = JSON.parse(localStorage.getItem('carrito')) //cargo los ids
     }
-    let productos = carrito.map(id => listaProductos.find(e => e.id === id)) //defino los productos
-    console.log(productos) //veo si se cargan
-    return productos
+    return ids.map(id => listaProductos.find(e => e.id === id)) //defino los productos
 }
 
-carrito = addToCartClicked()
+let carrito = showProducts() //cargo los productos
+console.log(carrito)
 
-
-
-
-
-function renderCarrito() {
-    carrito.map(producto => {
-        const Content = `
-      
-      <div class="row shoppingCartItem">
-      <div class="col-6">
-          <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-              <img src=${producto.image} class="shopping-cart-image">
-              <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${producto.nombre}</h6>
-          </div>
-      </div>
-      <div class="col-2">
-          <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-              <p class="item-price mb-0 shoppingCartItemPrice">${producto.precio}</p>
-          </div>
-      </div>
-      <div class="col-4">
-          <div
-              class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-              <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                  value="1">
-              <button class="btn btn-danger buttonDelete" type="button">X</button>
-          </div>
-      </div>
-  </div>
-      
-      `
-        shoppingCartItemsContainer.innerHTML = Content;
-
-
-        shoppingCartItemsContainer.querySelector(".buttonDelete").addEventListener('click', deleteCartItem)
-    })
-    function deleteCartItem(){
-        localStorage.removeItem(carrito);
-        renderCarrito()
-    }
+function htmlCarrito(products) {
+    return products.map(prod => (`
+        <div class="row shoppingCartItem">
+            <div class="col-6">
+                <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                    <img src=${prod.image} class="shopping-cart-image">
+                    <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${prod.nombre}</h6>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                    <p class="item-price mb-0 shoppingCartItemPrice">${prod.precio}</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div
+                    class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                    <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                        value="1">
+                    <button class="btn btn-danger buttonDelete" type="button">X</button>
+                </div>
+            </div>
+        </div>
+    `)).join('h')
 }
-renderCarrito()
+console.log(htmlCarrito(carrito))
+
+document.getElementById('shoppingCartItemsContainer').innerHTML = htmlCarrito(carrito)
