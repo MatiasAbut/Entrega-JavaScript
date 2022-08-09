fc523bc2ddc669047838496925c4da7b4d645b69
Matias Abut <mat.abut@hotmail.com>
2022-08-09 07:58:52 -0300

    Mostrando el total

--- JS/carrito.js
+++ JS/carrito.js
@@ -11,7 +11,7 @@ console.log(carrito)
 
 function htmlCarrito(products) {
     return products.map(prod => (`
-        <div class="row shoppingCartItem">
+        <div class="row shoppingCartItem" id="show${prod.id}">
             <div class="col-6">
                 <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
                     <img src=${prod.image} class="shopping-cart-image">
@@ -28,32 +28,25 @@ function htmlCarrito(products) {
                     class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
                     <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
                         value="1">
-                    <button class="btn btn-danger buttonDelete" type="button">X</button>
+                    <button class="btn btn-danger buttonDelete" type="button" onclick="removeShoppingCartItem('${prod.id}')">X</button>
                 </div>
             </div>
         </div>
     `)).join('')
-
-
 }
-
-
-
-
-
 
 let impresion = htmlCarrito(carrito)
 console.log(impresion)
 
-document.getElementById('shoppingCartItemsContainer').innerHTML = impresion
-
 document
-.querySelector('.buttonDelete')
-.addEventListener('click', removeShoppingCartItem);
+  .getElementById('shoppingCartItemsContainer').innerHTML = impresion
 
 document
 .querySelector('.shoppingCartItemQuantity')
 .addEventListener('change', quantityChanged);
+
+updateShoppingCartTotal()
+
 
 function updateShoppingCartTotal() {
     let total = 0;
@@ -80,10 +73,15 @@ function updateShoppingCartTotal() {
   }
 
 
-function removeShoppingCartItem(event) {
-    const buttonClicked = event.target;
-    buttonClicked.closest('.shoppingCartItem').remove();
-    updateShoppingCartTotal();
+
+  function removeShoppingCartItem(id) {
+    carrito = carrito.filter(e => e.id !== id)
+    console.log(carrito)
+    let ids = carrito.map(e => e.id)
+    localStorage.setItem('carrito', JSON.stringify(ids)) //vuelvo a guardar en el storage el carrito, sino no se borra nunca del storage
+    console.log(document.getElementById(`show${id}`))
+    document.getElementById(`show${id}`).innerHTML = ""
+    
   }
   
   function quantityChanged(event) {
